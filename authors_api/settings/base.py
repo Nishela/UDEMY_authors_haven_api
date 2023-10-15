41ac2d3acb9bd3f88d22d3e2fcd7778c062eee8b
Sergei Aleshin <sergei.aleshin@alludo.com>
2023-10-15 12:03:45 +0400

    add: celery, redis, flower

--- authors_api/settings/base.py
+++ authors_api/settings/base.py
@@ -40,6 +40,7 @@ THIRD_PARTY_APPS = [
     "phonenumber_field",
     "drf_yasg",
     "corsheaders",
+    "djcelery_email",
 ]
 
 LOCAL_APPS = [
@@ -173,3 +174,14 @@ LOGGING = {
 }
 
 AUTH_USER_MODEL = "users.User"
+
+CELERY_BROKER_URL = env("CELERY_BROKER")
+CELERY_RESULT_BACKEND = env("CELERY_BROKER")
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_RESULT_BACKEND_MAX_RETRIES = 10
+CELERY_TASK_SEND_SENT_EVENT = True
+
+if USE_TZ:
+    CELERY_TIMEZONE = TIME_ZONE
