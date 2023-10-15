0b23f310845396dcbbefc0c0463faab0ff308379
Sergei Aleshin <sergei.aleshin@alludo.com>
2023-10-15 15:33:27 +0400

    add: jwt auth

--- authors_api/settings/base.py
+++ authors_api/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 import environ
 
 env = environ.Env()
@@ -41,6 +42,12 @@ THIRD_PARTY_APPS = [
     "drf_yasg",
     "corsheaders",
     "djcelery_email",
+    "rest_framework.authtoken",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "dj_rest_auth",
+    "dj_rest_auth.registration",
 ]
 
 LOCAL_APPS = [
@@ -64,6 +71,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = "authors_api.urls"
@@ -175,3 +183,46 @@ CELERY_TASK_SEND_SENT_EVENT = True
 
 if USE_TZ:
     CELERY_TIMEZONE = TIME_ZONE
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+    ]
+}
+
+SIMPLE_JWT = {
+    "AUTH_HEADER_TYPES": ("Bearer",),  # Ожидаемый тип заголовка для JWT токена.
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": True,
+    "SIGNING_KEY": env("SIGNING_KEY"),  # Секретный ключ для подписи JWT токена.
+    "USER_ID_FIELD": "id",  # Поле модели пользователя, которое будет использоваться для идентификации пользователя.
+    "USER_ID_CLAIM": "user_id",  # Ключ в токене, который будет использоваться для идентификации пользователя.
+}
+
+REST_AUTH = {
+    "USE_JWT": True,  # Использовать JWT токен для аутентификации.
+    "JWT_AUTH_COOKIE": "authors-access-token",
+    "JWT_AUTH_REFRESH_COOKIE": "authors-refresh-token",
+    "REGISTER_SERIALIZER": "core_apps.users.serializers.CustomRegisterSerializer",
+    # Использовать кастомный сериализатор для регистрации.
+}
+
+AUTHENTICATION_BACKENDS = [
+    "allauth.account.auth_backends.AuthenticationBackend",
+    "django.contrib.auth.backends.ModelBackend",
+]
+
+ACCOUNT_AUTHENTICATION_METHOD = "email"  # Использовать email для аутентификации.
+ACCOUNT_EMAIL_REQUIRED = True  # Обязательно указывать email при регистрации.
+ACCOUNT_EMAIL_VERIFICATION = "mandatory"  # Обязательно подтверждать email.
+ACCOUNT_CONFIRM_EMAIL_ON_GET = True  # Подтверждать email при переходе по ссылке.
+ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 1  # Срок действия ссылки для подтверждения email.
+ACCOUNT_USER_MODEL_USERNAME_FIELD = None  # Не использовать username.
+ACCOUNT_USERNAME_REQUIRED = False  # Не использовать username.
--- core_apps/users/views.py
+++ core_apps/users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from .serializers import UserSerializer
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+class CustomUserDetailsView(generics.RetrieveUpdateAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
+
+    def get_queryset(self):
+        return get_user_model().objects.none()
