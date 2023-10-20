172cb499475b5151b5bc3b44c77eefe20f970aa0
Sergei Aleshin <sergei.aleshin@alludo.com>
2023-10-20 13:24:55 +0400

    add: article functional

---
+++ core_apps/articles/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+
+from . import models
+
+
+@admin.register(models.Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["pkid", "author", "title", "slug", "view_count"]
+    list_display_links = ["pkid", "author"]
+    list_filter = ["created_at", "updated_at"]
+    search_fields = ["title", "body", "tags"]
+    ordering = ["-created_at"]
+
+
+@admin.register(models.ArticleView)
+class ArticleViewAdmin(admin.ModelAdmin):
+    list_display = ["pkid", "article", "user", "viewer_ip"]
+    list_display_links = ["pkid", "article"]
+    list_filter = ["created_at", "updated_at"]
+    search_fields = ["article", "user", "viewer_ip"]
+
+# class ClapAdmin(admin.ModelAdmin):
+#     list_display = ["pkid", "id", "user", "article"]
+#     list_display_links = ["id", "user"]
+#     list_filter = ["created_at", "updated_at"]
