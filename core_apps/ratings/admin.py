cb85a22d0624a7b5ee0e933b461a0180a8061c8b
Sergei Aleshin <sergei.aleshin@alludo.com>
2023-10-20 14:37:32 +0400

    add: article rating

--- core_apps/ratings/admin.py
+++ core_apps/ratings/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Rating
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "article", "rating", "created_at", "updated_at"]
